58608bc0ff5e082dd984c8cdbd2d2b1afe043c78
bas123s <93962379+bas123s@users.noreply.github.com>
2021-11-26 11:15:39 +0700

    dd

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Snowfall from 'react-snowfall'
+import react from "react"
+
 
 
 export default function Home() {
   return (
     <div >
-      <Snowfall color={settings.color} snowflakeCount={settings.snowflakeCount} />
+     
      <div className='bg-one'>
 
 
