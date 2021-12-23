9cc51867747383f6f8daa17291eb6d63797029c5
bas123s <93962379+bas123s@users.noreply.github.com>
2021-12-23 11:09:52 +0700

    gg

--- components/count.js
+++ components/count.js
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import { useEffect, useState } from "react";
-import Par from '../pages/Par'
+import Par from "../pages/Par";
 
 export default function Count() {
   useEffect(() => {
@@ -34,41 +34,45 @@ export default function Count() {
   return (
     <div>
       <div className="time-wrapper">
-      
-      {partyTime ? (
-        <div>
-        <div className="  w-full  fixed top-0 left-0  flex items-center justify-center  bg-opacity-50 transform transition-transform duration-300 insert-0 text-right inline-block  px-4 py-2 leading-none  text-white  w-screen h-screen  ">
-        <a href='#'className="z-40 text-7xl text-white z-50 close rounded-lg" ><span> Happy New Year ! </span></a>
-     
+        {partyTime ? (
+          <div>
+            <div className="  w-full  fixed top-0 left-0  flex items-center justify-center  bg-opacity-50 transform transition-transform duration-300 insert-0 text-right inline-block  px-4 py-2 leading-none  text-white  w-screen h-screen  ">
+              <a
+                href="#"
+                className="z-40 text-7xl text-white z-50 close rounded-lg"
+              >
+                <span> Happy New Year ! </span>
+              </a>
 
-          <a href='/End'className="text-2xl text-white z-50 close rounded-lg" ><span> Close </span></a>
+              <a
+                href="/End"
+                className="text-2xl text-white z-50 close rounded-lg"
+              >
+                <span> Close </span>
+              </a>
 
-   
-
-         <div className="z-0"> <Par/></div>
-     
-        </div></div>
+              <div className="z-0">
+                {" "}
+                <Par />
+              </div>
+            </div>
+          </div>
         ) : (
           <div className="timer-inner  ">
             <div className=" button timer-segment ">
-              <span className="time text-white ">{days}</span>
-              <span className="label px-1 text-white">Days</span>
+              <span className="time text-white ">{days} Days</span>
             </div>
 
             <div className=" button timer-segment ">
-              <span className="time text-white ">{hours}</span>
-              <span className="label px-1 text-white">Hour</span>
+              <span className="time text-white ">{hours} Hours</span>
             </div>
 
-            <div className=" button timer-segment visible sm:invisible md:invisible lg:visible">
-              <span className="time text-white   ">{minutes}</span>
-              <span className="label px-1 text-white">Min</span>
+            <div className=" button timer-segment visible ">
+              <span className="time text-white   ">{minutes} Mins</span>
             </div>
 
             <div className="button timer-segment">
-              <div className="time text-white  w-20">{seconds}</div>
-        
-              
+              <div className="time text-white  ">{seconds} Secs</div>
             </div>
           </div>
         )}
